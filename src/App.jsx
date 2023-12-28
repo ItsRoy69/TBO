11f8a0bd41070b50baa150cbece7fe2d6c35c334
itsroy69 <jyotirmoyroy649@gmail.com>
2023-12-28 23:41:20 +0530

    added initial files
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LandingPage1 from "./pages/landingpage/LandingPage1";
+
+function App() {
+  return (
+    <>
+      <LandingPage1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/landingpage/LandingPage1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LandingPage1 = () => {
+  return (
+    <div>LandingPage1</div>
+  )
+}
+
+export default LandingPage1
