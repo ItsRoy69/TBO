25deb35b9d18b50901ad531e19ceaf396f14b7df
soumikbaksi18 <baksisoumik@gmail.com>
2024-01-01 04:53:23 +0530

    Emergency section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import LandingPage from "./pages/landingpage/LandingPage";
 import LandingPage2 from "./pages/landingpage/LandingPage2";
 import LandingPage3 from "./pages/landingpage/LandingPage3";
+import LandingPage4 from "./pages/landingpage/LandingPage4";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route exact path="/" element={<LandingPage />}/>
       <Route exact path="/room" element={<LandingPage2 />}/>
       <Route exact path="/cab" element={<LandingPage3 />}/>
+      <Route exact path="/emergency" element={<LandingPage4 />}/>
       </Routes>
     </>
   );
